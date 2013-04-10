a5765e4624de28b9a501425200cf8f47faa9751f
dchandekstark <dchandekstark@gmail.com>
2013-04-10 13:45:04 -0400

    Object properties added to show page

--- app/controllers/fcrepo_admin/objects_controller.rb
+++ app/controllers/fcrepo_admin/objects_controller.rb
@@ -1,9 +1,13 @@
 module FcrepoAdmin
   class ObjectsController < ApplicationController
+    
+    PROPERTIES = [:owner_id, :state, :create_date, :modified_date, :label]
 
     def show
       @object = ActiveFedora::Base.find(params[:id], :cast => true)
       authorize! :read, @object
+      @properties = {}
+      PROPERTIES.each { |p| @properties[p] = @object.send(p) }
     end
 
   end
--- spec/features/objects/show.html.erb_spec.rb
+++ spec/features/objects/show.html.erb_spec.rb
@@ -4,6 +4,14 @@ describe "objects/show.html.erb" do
   after { object.delete }
   context "basic object" do
     let(:object) { FactoryGirl.create(:content_model) }
+    it "should display the object's properties" do
+      visit fcrepo_admin.object_path(object)
+      page.should have_content(I18n.t("fcrepo_admin.object.properties.keys.state"))
+      page.should have_content(I18n.t("fcrepo_admin.object.properties.keys.create_date"))
+      page.should have_content(I18n.t("fcrepo_admin.object.properties.keys.modified_date"))
+      page.should have_content(I18n.t("fcrepo_admin.object.properties.keys.owner_id"))
+      page.should have_content(I18n.t("fcrepo_admin.object.properties.keys.label"))
+    end
     it "should link to all datastreams" do
       visit fcrepo_admin.object_path(object)
       object.datastreams.reject { |dsid, ds| ds.profile.empty? }.each_key do |dsid|
