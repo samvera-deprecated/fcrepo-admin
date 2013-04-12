2bbc975dc9febaee84ff633d62b38a1e70a4a95d
dchandekstark <dchandekstark@gmail.com>
2013-04-12 13:17:55 -0400

    didn't need to require cancan here

--- lib/fcrepo_admin.rb
+++ lib/fcrepo_admin.rb
@@ -1,7 +1,6 @@
 require 'fcrepo_admin/engine'
 require 'blacklight'
 require 'hydra/head'
-require 'cancan'
 
 module FcrepoAdmin
   autoload :SolrDocumentExtension, 'fcrepo_admin/solr_document_extension'
