93eef78e190d71ddc12ab16c4e138b0fde5b5aca
dchandekstark <dchandekstark@gmail.com>
2013-04-13 20:32:04 -0400

    Closes #12

--- lib/fcrepo_admin/datastream_ability.rb
+++ lib/fcrepo_admin/datastream_ability.rb
@@ -2,8 +2,13 @@ require 'cancan'
 
 module FcrepoAdmin
   module DatastreamAbility
+    extend ActiveSupport::Concern
+    
+    included do
+      self.ability_logic += [:datastream_permissions]
+    end
 
-    def custom_permissions
+    def datastream_permissions
       can :read, ActiveFedora::Datastream do |ds|
         test_read(ds.pid)
       end 
