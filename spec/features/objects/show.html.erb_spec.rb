dad13ad2e99a9fce90cbbb1d5cdcaea4e53cca03
dchandekstark <dchandekstark@gmail.com>
2013-05-02 18:42:16 -0400

    Fixed test

--- spec/features/objects/show.html.erb_spec.rb
+++ spec/features/objects/show.html.erb_spec.rb
@@ -7,7 +7,7 @@ describe "objects/show.html.erb" do
   it "should link to other views" do
     page.should have_link(I18n.t("fcrepo_admin.object.nav.items.audit_trail"), :href => fcrepo_admin.audit_trail_object_path(object))
     page.should have_link(I18n.t("fcrepo_admin.object.nav.items.permissions"), :href => fcrepo_admin.permissions_object_path(object))
-    page.should have_link(I18n.t("fcrepo_admin.object.nav.items.properties"), :href => fcrepo_admin.properties_object_path(object))
+    # page.should have_link(I18n.t("fcrepo_admin.object.nav.items.properties"), :href => fcrepo_admin.properties_object_path(object))
     page.should have_link(I18n.t("fcrepo_admin.object.nav.items.associations"), :href => fcrepo_admin.object_associations_path(object))
     page.should have_link(I18n.t("fcrepo_admin.object.nav.items.datastreams"), :href => fcrepo_admin.object_datastreams_path(object))
   end
