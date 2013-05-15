1a2568537443f222ec65bc9083799f5f9ef247ef
dchandekstark <dchandekstark@gmail.com>
2013-05-15 18:04:16 -0400

    Refactored helpers

--- lib/fcrepo_admin/helpers/datastreams_helper_behavior.rb
+++ lib/fcrepo_admin/helpers/datastreams_helper_behavior.rb
@@ -1,27 +1,87 @@
 module FcrepoAdmin::Helpers
   module DatastreamsHelperBehavior
-    include FcrepoAdmin::Helpers::FcrepoAdminHelperBehavior
+
+    def datastream_title
+      "#{t('fcrepo_admin.datastream.title')}: #{@datastream.dsid}"
+    end
 
     def datastream_context_nav_header
       t("fcrepo_admin.datastream.nav.header")
     end
 
     def datastream_context_nav_items
-      render :partial => 'fcrepo_admin/datastreams/context_nav_items', :locals => {:object => @object, :datastream => @datastream}
+      FcrepoAdmin.datastream_context_nav_items.collect do |item| 
+        content = datastream_context_nav_item(item)
+        content unless content.nil?
+      end
     end
 
-    # List of ds profile keys for ds index view
     def datastream_index_columns
-      ["dsLabel", "dsMIME", "dsSize", "dsCreateDate"]
+      FcrepoAdmin.datastream_index_columns
     end
 
-    # List of ds profile keys for ds history view
     def datastream_history_columns
-      ["dsCreateDate"]
+      FcrepoAdmin.datastream_history_columns
     end
 
     def datastream_params
       params.has_key?(:asOfDateTime) ? {:asOfDateTime => params[:asOfDateTime]} : {}
+    end
+
+    def datastream_context_nav_item(item)
+      condition = case
+                  when item == :current_version then !@datastream.current_version?
+                  when item == :summary         then true
+                  when item == :content         then @datastream.content_is_text?
+                  when item == :download        then @datastream.content_is_downloadable?
+                  when item == :edit            then @datastream.content_is_editable? && can?(:edit, @object)
+                  when item == :upload          then @datastream.content_is_uploadable? && can?(:upload, @object)
+                  when item == :history         then !@datastream.new?
+                  end
+      custom_datastream_context_nav_item(item) if condition.nil?
+      link_to_datastream(item) if condition
+    end
+
+    def custom_datastream_context_nav_item(item)
+      # Override this method with your custom items
+    end
+
+    def link_to_datastream_version(dsVersion)
+      link_to_unless dsVersion.current_version?, dsVersion.dsVersionID, fcrepo_admin.object_datastream_path(@object, @datastream, :asOfDateTime => ds.asOfDateTime) do |name|
+        "#{name} (current version)"
+      end
+    end
+
+    def link_to_datastream(view)
+      path = case
+             when view == :current_version then fcrepo_admin.object_datastream_path(@object, @datastream)
+             when view == :summary         then fcrepo_admin.object_datastream_path(@object, @datastream, datastream_params)
+             when view == :content         then fcrepo_admin.content_object_datastream_path(@object, @datastream, datastream_params)
+             when view == :download        then fcrepo_admin.download_object_datastream_path(@object, @datastream, datastream_params)
+             when view == :edit            then fcrepo_admin.edit_object_datastream_path(@object, @datastream)
+             when view == :upload          then fcrepo_admin.upload_object_datastream_path(@object, @datastream)
+             when view == :history         then fcrepo_admin.history_object_datastream_path(@object, @datastream)
+             end
+      label = t("fcrepo_admin.datastream.nav.items.#{view}")
+      if view == :current_version || view == :download
+        link_to label, path
+      else
+        link_to_unless_current label, path
+      end
+    end
+
+    def render_datastream_version
+      render :partial => 'version', :locals => {:datastream => @datastream}
+    end
+
+    def format_ds_profile_value(ds, key)
+      value = ds.profile[key]
+      case
+      when key == "dsSize" then number_to_human_size(value)
+      when key == "dsCreateDate" then value.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
+      when key == "dsLocation" && (ds.external? || ds.redirect?) then link_to(value, value)
+      else value
+      end
     end
 
   end
--- lib/fcrepo_admin/helpers/objects_helper_behavior.rb
+++ lib/fcrepo_admin/helpers/objects_helper_behavior.rb
@@ -1,6 +1,5 @@
 module FcrepoAdmin::Helpers
   module ObjectsHelperBehavior
-    include FcrepoAdmin::Helpers::FcrepoAdminHelperBehavior
 
     def object_title
       "#{object_type} #{@object.pid}"
@@ -10,17 +9,51 @@ module FcrepoAdmin::Helpers
       @object.class.to_s
     end
 
-    def object_properties_keys
-      [:owner_id, :state, :create_date, :modified_date, :label]
-    end
-
     def object_properties
-      object_properties_keys.inject(Hash.new) { |h, p| h[p] = @object.send(p); h }
+      FcrepoAdmin.object_properties.inject(Hash.new) { |h, p| h[p] = @object.send(p); h }
     end
 
-    # List of ds profile keys for for object show view
     def object_show_datastream_columns
-      ["dsLabel"]
+      FcrepoAdmin.object_show_datastream_columns
+    end
+
+    def object_context_nav_header
+	  t("fcrepo_admin.object.nav.header")
+    end
+
+    def object_context_nav_items
+      FcrepoAdmin.object_context_nav_items.collect do |item|
+        content = object_context_nav_item(item)
+        content unless content.nil?
+      end
+    end
+
+    def object_context_nav_item(item)
+      condition = case
+                  when item == :summary      then true
+                  when item == :datastreams  then true
+                  when item == :permissions  then @object.has_permissions? && can?(:permissions, @object)
+                  when item == :associations then true
+                  when item == :audit_trail  then @object.auditable? && can?(:audit_trail, @object)
+                  end
+      custom_object_context_nav_item(item) if condition.nil?
+      link_to_object(item) if condition
+    end
+
+    def link_to_object(view)
+      label = t("fcrepo_admin.object.nav.items.#{view}")
+      path = case
+             when view == :summary      then fcrepo_admin.object_path(@object)
+             when view == :datastreams  then fcrepo_admin.object_datastreams_path(@object)
+             when view == :permissions  then fcrepo_admin.permissions_object_path(@object)
+             when view == :associations then fcrepo_admin.object_associations_path(@object)
+             when view == :audit_trail  then fcrepo_admin.audit_trail_object_path(@object)
+             end
+      link_to_unless_current label, path
+    end
+
+    def custom_object_context_nav_item(item)
+      # Override this method with your custom items
     end
     
   end
