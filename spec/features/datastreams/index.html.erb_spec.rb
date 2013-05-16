4ccbe7d93f8fc527ebd17dffffa3c09c0f9d80d0
dchandekstark <dchandekstark@gmail.com>
2013-05-16 14:42:26 -0400

    should link to all datastreams, not just those that are persisted

--- spec/features/datastreams/index.html.erb_spec.rb
+++ spec/features/datastreams/index.html.erb_spec.rb
@@ -4,9 +4,9 @@ describe "datastreams/index.html.erb" do
   before { visit fcrepo_admin.object_datastreams_path(object) }
   after { object.delete }
   let(:object) { FactoryGirl.create(:item) }
-  it "should link to all persisted datastreams" do
-    object.datastreams.reject { |dsid, ds| ds.profile.empty? }.each_key do |dsid|
-      page.should have_link(dsid, :href => fcrepo_admin.object_datastream_path(object, dsid))
+  it "should link to all datastreams" do
+    object.datastreams.each do |dsid, ds|
+      page.should have_link(dsid, :href => fcrepo_admin.object_datastream_path(object, ds))
     end
   end  
 end
