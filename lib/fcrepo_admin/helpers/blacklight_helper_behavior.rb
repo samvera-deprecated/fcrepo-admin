ad206d82eee3592344d51e9fad471638f85951e2
dchandekstark <dchandekstark@gmail.com>
2013-05-24 18:27:36 -0400

    trivial typographic adjustment

--- lib/fcrepo_admin/helpers/blacklight_helper_behavior.rb
+++ lib/fcrepo_admin/helpers/blacklight_helper_behavior.rb
@@ -1,7 +1,7 @@
 module FcrepoAdmin::Helpers
   module BlacklightHelperBehavior
 
-    def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
+    def link_to_document(doc, opts = {:label => nil, :counter => nil, :results_view => true})
       opts[:label] ||= blacklight_config.index.show_link.to_sym
       label = render_document_index_label doc, opts
       link_to label, fcrepo_admin.object_path(doc.id)
