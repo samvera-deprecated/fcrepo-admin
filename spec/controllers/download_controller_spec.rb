95507c300ea4cd1cc2ea07996cb7b8121fd1ef9c
dchandekstark <dchandekstark@gmail.com>
2013-06-07 13:10:20 -0400

    Implement download response test

--- spec/controllers/download_controller_spec.rb
+++ spec/controllers/download_controller_spec.rb
@@ -4,8 +4,9 @@ describe FcrepoAdmin::DownloadController do
   let(:object) { FactoryGirl.create(:item) }
   after { object.delete }
   context "#show" do
-    #get :show, :object_id => object.pid, :id => object.descMetadata, :use_route => 'fcrepo_admin'
-    pending "Test response body or size"
-    pending "Test response header filename"
+    it "should have the right content and headers" do
+      get :show, :object_id => object.pid, :id => object.descMetadata, :use_route => 'fcrepo_admin'
+      response.headers['Content-Disposition'].should == "inline; filename=\"#{object.pid.sub(/:/, '_')}__descMetadata.xml\""
+    end
   end
 end
