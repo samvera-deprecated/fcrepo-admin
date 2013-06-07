8c648b9acd7748f12eecbf531224058667e6ddf9
dchandekstark <dchandekstark@gmail.com>
2013-06-07 12:38:56 -0400

    Override #datastream_name

--- app/controllers/fcrepo_admin/download_controller.rb
+++ app/controllers/fcrepo_admin/download_controller.rb
@@ -1,3 +1,5 @@
+require 'mime/types'
+
 module FcrepoAdmin
   class DownloadController < ApplicationController
 
@@ -11,6 +13,17 @@ module FcrepoAdmin
 
     def datastream_to_show
       asset.datastreams[params[:id]]
+    end
+
+    # Pattern: pid__dsid.ext (replacing colon in pid with underscore)
+    def datastream_name
+      "#{datastream.pid.sub(/:/, '_')}__#{datastream.dsid}.#{datastream_extension}"
+    end
+
+    private
+
+    def datastream_extension
+      MIME::Types[datastream.mimeType].first.extensions.first rescue 'bin'
     end
 
   end
