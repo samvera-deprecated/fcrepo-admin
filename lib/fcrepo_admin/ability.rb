2d0c0a707a0e0deb6b2027c7721d2941567f7a0b
dchandekstark <dchandekstark@gmail.com>
2013-06-27 17:16:07 -0400

    Custom permissions are now set from configuration, not using aliases Method name changed

--- lib/fcrepo_admin/ability.rb
+++ lib/fcrepo_admin/ability.rb
@@ -3,16 +3,15 @@ module FcrepoAdmin
     extend ActiveSupport::Concern
     
     included do
-      self.ability_logic += [:fcrepo_admin_aliases]
+      self.ability_logic += [:fcrepo_admin_object_permissions]
     end
 
-    def fcrepo_admin_aliases
-      alias_action :download, :to => :read
-      alias_action :audit_trail, :to => :read
-      alias_action :permissions, :to => :read
-      alias_action :content, :to => :read
-      alias_action :history, :to => :read
-      alias_action :upload, :to => :update
+    def fcrepo_admin_object_permissions
+      FcrepoAdmin.object_permissions.each do |action, permission|
+        can action, ActiveFedora::Base do |obj|
+          send("test_#{permission}".to_sym, obj.pid)
+        end
+      end
     end
 
   end
